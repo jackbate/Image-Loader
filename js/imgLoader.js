67fe2ea08030d2b973060c8ac66b846a62097df0
jackbate <75363110+jackbate@users.noreply.github.com>
2021-09-22 00:27:05 +0100

    minor styling working image loader and also array loader working

--- js/imgLoader.js
+++ js/imgLoader.js
@@ -1,14 +1,48 @@
+var randomPic = 1;
+var removalHelp = 0;
+var src;
 
-var emailAddress = document.getElementById('email');
 
-$('#btn').on('click', function() {
-    var img = ""
-    var img = $('<img />', {src : 'http://webpage.com/images/' + $('#imagesize').val() +'.png'});
-    img.appendTo('body');
-    
-});
+var userImages = [];
 
-function formSubmit(event) {
-    event.preventDefault();
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this);
+
+
+document.getElementById('btn').onclick = function() {
+    var val = document.getElementById('imagesize').value,
+    src = 'https://picsum.photos/' + val + '?random=' + randomPic,
+    img = document.createElement('img');
+    img.src = src;
+    randomPic += 1;
+    removalHelp ++
+    if (removalHelp <=1){
+        document.getElementById('image_container').innerHTML += '<img src="'+img.src+'" id = "image"/>';
+        console.log('first stage complete')
+    }
+    else{
+       document.getElementById('image').src = src;
+
+
+        console.log("this is image " + removalHelp)
+    }
+    return src;
 }
+
+document.getElementById('savePhoto').onclick = function() {
+    let emailAddress = document.getElementById('email').value;
+
+    let addUserDetails = {
+        email: document.getElementById('email').value,
+        savedImage: document.getElementById('image').src
+    }
+    let emailCheck = userImages.includes(document.getElementById('email').value)
+    if (emailCheck == false){
+        userImages.push(addUserDetails);
+        console.log(emailCheck);
+    }else{
+        alert("added additional images to array")
+        console.log(emailCheck);
+    }
+    // document.forms[0].reset();
+    // userImages.push(addUserDetails);
+    console.log(userImages);
+}   
